c32c522b82b8d27402d6ff6b4f178f38682d340f
hamizannordin <hamizan.nordin@gmail.com>
2023-09-03 22:36:10 +0800

    test: add update postcode detail unit test

--- src/test/java/wcc/postcode/PostcodeServiceTest.java
+++ src/test/java/wcc/postcode/PostcodeServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
+import wcc.postcode.body.request.UpdatePostcodeDetailRequest;
 import wcc.postcode.body.response.DistanceBetweenPostcodeResponse;
 import wcc.postcode.repository.PostcodeRepository;
 import wcc.postcode.util.DistanceCalculatorUtil;
@@ -35,6 +36,7 @@ public class PostcodeServiceTest {
      */
     @Test
     public void getDistanceBetweenPostcodeFailParamIncomplete() {
+        
         String postcodeA = "";
         String postcodeB = "ABC 00";
         String message = "Requires two postcode";
@@ -49,6 +51,7 @@ public class PostcodeServiceTest {
      */
     @Test
     public void getDistanceBetweenPostcodeFailPostcodeNotFound() {
+        
         String postcodeA = "ABC 01a";
         String postcodeB = "ABC 01b";
         String message = "Postcode not found";
@@ -98,42 +101,92 @@ public class PostcodeServiceTest {
     }
 
     /**
-     * test-case 03 : updatePostcodeDetailFailPostcodeNullOrEmpty
+     * test-case 03 : updatePostcodeDetailFailPostcodeBlank
      */
-//    @Test
-//    public void updatePostcodeDetailFailPostcodeNullOrEmpty() {
-//        
-//    }
+    @Test
+    public void updatePostcodeDetailFailPostcodeBlank() {
+        
+        String postcode = " ";
+        String message = "Postcode contains no value";
+        UpdatePostcodeDetailRequest request = new UpdatePostcodeDetailRequest();
+        
+        assertTrue(assertThrows(BadRequestException.class,()->{
+            postcodeService.updatePostcodeDetail(postcode, request);
+        }).getMessage().equals(message));
+    }
 
     /**
-     * test-case 04 : updatePostcodeDetailFailLatLongNullOrEmpty
+     * test-case 04 : updatePostcodeDetailFailInvalidLatitudeValue
      */
-//    @Test
-//    public void updatePostcodeDetailFailLatLongNullOrEmpty() {
-//        
-//    }
+    @Test
+    public void updatePostcodeDetailFailInvalidLatitudeValue() {
+        
+        String postcode = "ABC 04";
+        String message = "Invalid latitude value";
+        UpdatePostcodeDetailRequest request = new UpdatePostcodeDetailRequest();
+        request.setLatitude(999999);
+        request.setLongitude(0.12345678);
+        
+        assertTrue(assertThrows(BadRequestException.class,()->{
+            postcodeService.updatePostcodeDetail(postcode, request);
+        }).getMessage().equals(message));
+    }
 
     /**
-     * test-case 05 : updatePostcodeDetailFailLatLongInvalidFormat
+     * test-case 05 : updatePostcodeDetailFailInvalidLongitudeValue
      */
-//    @Test
-//    public void updatePostcodeDetailFailLatLongInvalidFormat() {
-//        
-//    }
+    @Test
+    public void updatePostcodeDetailFailInvalidLongitudeValue() {
+        
+        String postcode = "ABC 05";
+        String message = "Invalid longitude value";
+        UpdatePostcodeDetailRequest request = new UpdatePostcodeDetailRequest();
+        request.setLatitude(1.234);
+        request.setLongitude(181.78);
+        
+        assertTrue(assertThrows(BadRequestException.class,()->{
+            postcodeService.updatePostcodeDetail(postcode, request);
+        }).getMessage().equals(message));
+    }
 
     /**
      * test-case 06 : updatePostcodeDetailFailPostcodeNotFound
      */
-//    @Test
-//    public void updatePostcodeDetailFailPostcodeNotFound() {
-//        
-//    }
+    @Test
+    public void updatePostcodeDetailFailPostcodeNotFound() {
+        
+        String postcode = "ABC 06";
+        String message = "Postcode not found";
+        UpdatePostcodeDetailRequest request = new UpdatePostcodeDetailRequest();
+        
+        assertTrue(assertThrows(NotFoundException.class,()->{
+            postcodeService.updatePostcodeDetail(postcode, request);
+        }).getMessage().equals(message));
+    }
 
     /**
      * test-case 07 : updatePostcodeDetailSuccess
      */
-//    @Test
-//    public void updatePostcodeDetailSuccess() {
-//        
-//    }
+    @Test
+    public void updatePostcodeDetailSuccess() {
+        
+        String postcode = "ABC 05";
+        double latitude = 1.234;
+        double longitude = 0.1234567;
+        UpdatePostcodeDetailRequest request = new UpdatePostcodeDetailRequest();
+        request.setLatitude(latitude);
+        request.setLongitude(longitude);
+        
+        Postcode postcodeEntity = new Postcode(postcode, latitude, longitude);
+        
+        when(postcodeRepository.findByPostcode(postcode)).thenReturn(postcodeEntity);
+        
+        when(postcodeRepository.save(postcodeEntity)).thenReturn(postcodeEntity);
+        
+        Postcode postcodeResponse = postcodeService.updatePostcodeDetail(postcode, request);
+        
+        assertTrue(postcodeResponse.getLatitude() == latitude);
+        assertTrue(postcodeResponse.getLongitude()== longitude);
+        assertTrue(postcodeResponse.getPostcode().equals(postcode));
+    }
 }
